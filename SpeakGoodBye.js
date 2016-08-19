583b7b686ffa472ae5ba71ab048d6b4385c41a1b
DanilM1 <daniil.maltsev.md@gmail.com>
2016-08-19 16:52:13 +0300

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,8 @@
+﻿(function (window) {
+var byeSpeaker = {};
+byeSpeaker.speakWord = "Good Bye";
+byeSpeaker.sayGoodBye = function speak(name) {
+  console.log(byeSpeaker.speakWord + " " + name);
+}
+window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ SpeakHello.js
@@ -0,0 +1,8 @@
+﻿(function (window) {
+var helloSpeaker = {};
+helloSpeaker.speakWord = "Hello";
+helloSpeaker.sayHello = function speak(name) {
+  console.log(helloSpeaker.speakWord + " " + name);
+}
+window.helloSpeaker = helloSpeaker;
+})(window);
